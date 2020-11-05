d666c7aa9dab7a28f55b1483acfcef77e6f88609
samehsy <samehsy16@gmail.com>
2020-11-05 23:49:14 +0200

    make it better

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(fileUpload({
 }));
 
 
-var server = app.listen(3000, function () {
+var server = app.listen( process.env.PORT ||3000, function () {
     console.log('Example app listening on port ');
 });
 
